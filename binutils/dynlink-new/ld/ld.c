a41f4538a1136c921dd623484cf8df41a5e05b9b
TurtleTony <tristan.schoenhals@gmail.com>
2021-04-20 12:39:14 +0200

    Implement basic option handling using getopt

--- binutils/dynlink-new/ld/ld.c
+++ binutils/dynlink-new/ld/ld.c
@@ -4,10 +4,43 @@
  */
 
 #include <stdio.h>
+#include <ctype.h>
+#include <unistd.h>
 
 #include "eof.h"
+#include "ld.h"
 
-int main(void) {
+int main(int argc, char *argv[]) {
+
+    char *infile = NULL;
+    char *outfile = DEFAULT_OUT_FILE_NAME;
+
+    int c;
+    // Getopt keeps this easily expandable for the future
+    while ((c = getopt(argc, argv, "o:?")) != -1) {
+        switch (c) {
+            case 'o':
+                outfile = optarg;
+                break;
+            case '?':
+                print_usage();
+                return 1;
+            default:
+                return 1;
+        }
+    }
+
+    if (optind >= argc) {
+        print_usage();
+        return 1;
+    }
+
+    infile = argv[optind];
+
     printf("Hello World!\n");
     return 0;
+}
+
+void print_usage(void) {
+    printf("usage: ./ld [-o <outfile>] <infile>\n");
 }
---
+++ binutils/dynlink-new/ld/ld.h
@@ -0,0 +1,14 @@
+//
+// Created by tony on 20.04.21.
+//
+
+#ifndef ECO32_LD_H
+#define ECO32_LD_H
+
+
+
+#define DEFAULT_OUT_FILE_NAME "a.out"
+
+void print_usage(void);
+
+#endif //ECO32_LD_H
