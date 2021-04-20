b1f82fe228fbaa808744c08cbf91a83523151b23
TurtleTony <tristan.schoenhals@gmail.com>
2021-04-20 14:28:39 +0200

    Copy structs from ex02 example ld

--- binutils/dynlink-new/ld/ld.h
+++ binutils/dynlink-new/ld/ld.h
@@ -10,7 +10,49 @@
 #define DEFAULT_OUT_FILE_NAME "a.out"
 
 typedef struct module {
+    char *name;			/* module name */
+    unsigned char *data;		/* data space */
+    char *strs;			/* string space */
+    int nsegs;			/* number of segments */
+    struct segment *segs;		/* array of segments */
+    int nsyms;			/* number of symbols */
+    struct symbol *syms;		/* array of symbols */
+    int nrels;			/* number of relocations */
+    struct reloc *rels;		/* array of relocations */
 } Module;
+
+typedef struct segment {
+    char *name;			/* segment name */
+    unsigned char *data;		/* segment data */
+    unsigned int addr;		/* virtual start address */
+    unsigned int size;		/* size of segment in bytes */
+    unsigned int attr;		/* segment attributes */
+    /* used for output only */
+    unsigned int nameOffs;	/* offset in string space */
+    unsigned int dataOffs;	/* offset in segment data */
+} Segment;
+
+
+typedef struct symbol {
+    char *name;			/* symbol name */
+    int val;			/* the symbol's value */
+    int seg;			/* the symbol's segment, -1: absolute */
+    unsigned int attr;		/* symbol attributes */
+    /* used for output only */
+    unsigned int nameOffs;	/* offset in string space */
+} Symbol;
+
+
+typedef struct reloc {
+    unsigned int loc;		/* where to relocate */
+    int seg;			/* in which segment */
+    int typ;			/* relocation type: one of RELOC_xxx */
+    /* symbol flag RELOC_SYM may be set */
+    int ref;			/* what is referenced */
+    /* if symbol flag = 0: segment number */
+    /* if symbol flag = 1: symbol number */
+    int add;			/* additive part of value */
+} Reloc;
 
 Module *readModule(char *infile);
 void writeModule(Module *module, char *outfile);
