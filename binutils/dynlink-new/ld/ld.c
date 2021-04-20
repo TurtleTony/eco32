318e37446f91d970e7e81731b17ee9e3327c54b5
TurtleTony <tristan.schoenhals@gmail.com>
2021-04-20 15:12:08 +0200

    Reformat code

--- binutils/dynlink-new/ld/ld.c
+++ binutils/dynlink-new/ld/ld.c
@@ -134,16 +134,16 @@ void writeModule(Module *module, char *outfile) {
 uint32_t read4FromEco(unsigned char *p) {
     return (uint32_t) p[0] << 24 |
            (uint32_t) p[1] << 16 |
-           (uint32_t) p[2] <<  8 |
-           (uint32_t) p[3] <<  0;
+           (uint32_t) p[2] << 8 |
+           (uint32_t) p[3] << 0;
 }
 
 
 void write4ToEco(unsigned char *p, uint32_t data) {
     p[0] = data >> 24;
     p[1] = data >> 16;
-    p[2] = data >>  8;
-    p[3] = data >>  0;
+    p[2] = data >> 8;
+    p[3] = data >> 0;
 }
 
 
@@ -151,14 +151,14 @@ void conv4FromEcoToNative(unsigned char *p) {
     uint32_t data;
 
     data = read4FromEco(p);
-    * (uint32_t *) p = data;
+    *(uint32_t *) p = data;
 }
 
 
 void conv4FromNativeToEco(unsigned char *p) {
     uint32_t data;
 
-    data = * (uint32_t *) p;
+    data = *(uint32_t *) p;
     write4ToEco(p, data);
 }
 
