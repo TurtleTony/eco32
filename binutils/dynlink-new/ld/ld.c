7d375d0418c5091d57fcf4ed8234aebec627de1a
TurtleTony <tristan.schoenhals@gmail.com>
2021-04-20 11:45:23 +0200

    Add Makefile and hello world for exercise 2

---
+++ binutils/dynlink-new/ld/ld.c
@@ -0,0 +1,13 @@
+/*
+ * ld.c -- ECO32 link editor
+ * Created by tony on 20.04.21.
+ */
+
+#include <stdio.h>
+
+#include "eof.h"
+
+int main(void) {
+    printf("Hello World!\n");
+    return 0;
+}
