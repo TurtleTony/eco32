7b3d184cca0b278e6f1d6bea62a75486b31f636b
TurtleTony <tristan.schoenhals@gmail.com>
2021-06-05 15:58:28 +0200

    Update ld eof
    Only needed for better syntax highlighting

--- binutils/dynlink-new/binutils/ld/eof.h
+++ binutils/dynlink-new/binutils/ld/eof.h
@@ -8,7 +8,9 @@
 #define _EOF_H_
 
 
-#define EOF_MAGIC	0x8F0B45C0
+#define EOF_R_MAGIC	0xE7B79BFE	/* relocatable object */
+#define EOF_X_MAGIC	0x3AECCFDF	/* executable object */
+#define EOF_D_MAGIC	0xFB23146C	/* dynamic library object */
 
 #define SEG_ATTR_X	0x01	/* executable */
 #define SEG_ATTR_W	0x02	/* writable */
@@ -22,11 +24,17 @@
 #define RELOC_R16	2	/* write 16 bits with value relative to PC */
 #define RELOC_R26	3	/* write 26 bits with value relative to PC */
 #define RELOC_W32	4	/* write full 32 bit word with value */
+#define RELOC_GA_H16	5	/* PIC: GOT addr, PC relative, high part */
+#define RELOC_GA_L16	6	/* PIC: GOT addr, PC relative, low part */
+#define RELOC_GR_H16	7	/* PIC: GOT relative, high part */
+#define RELOC_GR_L16	8	/* PIC: GOT relative, low part */
+#define RELOC_GP_L16	9	/* PIC: GOT pointer, offset within GOT */
+#define RELOC_ER_W32	10	/* PIC: executable relative, 32 bit word */
 #define RELOC_SYM	0x100	/* symbol flag, may be added to any RELOC */
 
 
 typedef struct {
-  unsigned int magic;		/* must be EOF_MAGIC */
+  unsigned int magic;		/* must be one of EOF_<type>_MAGIC */
   unsigned int osegs;		/* offset of segment table in file */
   unsigned int nsegs;		/* number of segment table entries */
   unsigned int osyms;		/* offset of symbol table in file */
@@ -61,9 +69,11 @@ typedef struct {
 typedef struct {
   unsigned int loc;		/* where to relocate */
   int seg;			/* in which segment */
+				/* if -1: none, loc is virtual address */
   int typ;			/* relocation type: one of RELOC_xxx */
 				/* symbol flag RELOC_SYM may be set */
   int ref;			/* what is referenced */
+				/* if -1: nothing */
 				/* if symbol flag = 0: segment number */
 				/* if symbol flag = 1: symbol number */
   int add;			/* additive part of value */
