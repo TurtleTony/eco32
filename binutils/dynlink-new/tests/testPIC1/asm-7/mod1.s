975895a3a578d7dbd3d1bc32079f20ce244bd263
TurtleTony <tristan.schoenhals@gmail.com>
2021-06-05 15:34:47 +0200

    Add PIC tests

---
+++ binutils/dynlink-new/tests/testPIC1/asm-7/mod1.s
@@ -0,0 +1,16 @@
+	.export	func
+
+	.import	dat1
+	.import	dat2
+	.import	dat3
+	.import	dat4
+	.import	dat5
+
+	.code
+
+func:	.ldgot	$3
+	ldw	$8,$0,dat3
+	ldw	$9,$0,dat4
+	ldw	$10,$0,dat3
+	ldw	$11,$0,dat4
+loop:	j	loop
