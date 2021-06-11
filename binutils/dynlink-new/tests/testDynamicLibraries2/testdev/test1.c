50f3ea95f525823d92e883fe4e4141d1c942177d
Jan Martens <jan.niklas.martens@mni.thm.de>
2021-06-11 18:40:15 +0200

    Add dynamic test

---
+++ binutils/dynlink-new/tests/testDynamicLibraries2/testdev/test1.c
@@ -0,0 +1,15 @@
+/*
+ * test1.c -- an application using a library
+ */
+
+
+#include <stdio.h>
+#include "mylib.h"
+
+
+int main(int argc, char *argv[]) {
+    puts("calling a library function");
+    myfunc();
+    puts("returned from library function");
+    return 0;
+}
