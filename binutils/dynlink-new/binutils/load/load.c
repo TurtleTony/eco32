2fa300e982709a6f9b5b57da2e63bf5800d15a94
TurtleTony <tristan.schoenhals@gmail.com>
2021-07-05 14:36:14 +0200

    Prevent fread calls when size is 0

--- binutils/dynlink-new/binutils/load/load.c
+++ binutils/dynlink-new/binutils/load/load.c
@@ -234,7 +234,7 @@ void loadLinkUnit(char *name, unsigned int expectedMagic, FILE *inputFile, char 
                     error("cannot seek to data of segment '%s' in file '%s'",
                           strs + segmentRecord.name, inputPath);
                 }
-                if (fread(dataPointer, 1, segmentRecord.size, inputFile) != segmentRecord.size) {
+                if (segmentRecord.size != 0 && fread(dataPointer, segmentRecord.size, 1, inputFile) != 1) {
                     error("cannot read data of segment '%s' in file '%s'",
                           strs + segmentRecord.name, inputPath);
                 }
